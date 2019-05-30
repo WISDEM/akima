65c7c9be2b09170befe769bc26c0de2792139ad8
gbarter <gbarter@alum.mit.edu>
2019-05-30 09:52:18 -0600

    fixed compile errors

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ setup(
     package_dir={'': 'src'},
     py_modules=['akima'],
     license='Apache License, Version 2.0',
-    ext_modules=[Extension('_akima', ['src/akima.f90'], extra_f90_compile_args=['-O3'])]
+    ext_modules=[Extension('_akima', ['src/akima.f90'], extra_f90_compile_args=['-O3','-fPIC','-shared'], extra_link_args=['-shared'])]
 )
